b3cc3edaa4377f9b0da8142353e2e1606c463258
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-07-02 01:26:18 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,10 +4,9 @@ sudo apt-get update -y
 sudo apt-ger upgrade -y
 
 #to install my packages
-sudo apt-get install bluefish vokoscreen openbox obconf wireshark aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch
+sudo apt-get install bluefish vokoscreen openbox obconf wireshark aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox lxpanel irssi
+sudo apt-get install firefox tor-browser-launcher nano vim sudo open-ssh-server python3-pip git npm hydra networkmanager
 
-
-
-
+#prints end of program
 clear
 figlet "done"
