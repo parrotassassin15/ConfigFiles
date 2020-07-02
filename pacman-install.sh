780dc7910ca3dc50feb2c929a71547df83ec55c3
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-07-02 01:37:23 +0000

    Update pacman-install.sh

--- pacman-install.sh
+++ pacman-install.sh
@@ -5,4 +5,9 @@ sudo pacman -Syyy
 sudo pacman -Syu
 
 #installs my packages
-sudo pacman -S 
+sudo pacman -S bluefish vokoscreen openbox obconf wireshark-qt aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox lxpanel irssi
+firefox nano vim sudo openssh-server python3-pip git hydra networkmanager
+
+#prints end of program
+clear
+figlet "done"
