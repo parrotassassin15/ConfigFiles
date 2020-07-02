07e36cb6788943dd63698d0ac85fe286fc26b4db
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-07-02 01:08:39 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,10 +2,10 @@
 
 
 #install for apt package manager
-
+sudo apt install bluefish 
 
 #install for pacman package manager
-
+sudo pacman -S bluefish
 
 #install for yay AUR helper
-
+yay -S codelite bluefish 
