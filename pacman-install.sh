13b1416f5ba1b18196699b7016ff0ba1235b9021
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-08-30 03:32:35 +0000

    Update pacman-install.sh

--- pacman-install.sh
+++ pacman-install.sh
@@ -4,10 +4,27 @@
 sudo pacman -Syyy
 sudo pacman -Syu
 
+
+#installs yay (make sure the user is changed to your user)
+sudo pacman -S git
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R jok3rtwin2:jok3rtwin2 ./yay-git
+cd yay-git
+makepkg -si
+
+
 #installs my packages
-sudo pacman -S bluefish vokoscreen openbox obconf wireshark-qt aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox lxpanel irssi
-firefox nano vim sudo openssh-server python3-pip git hydra networkmanager
+sudo yay -S bluefish firefox virtualbox xfce4-terminal wireshark nmap dirbuster mpv compton xmonad xmobar xmonad-utils dmenu 
+sudo yay -S firefox firejail tor openssh-server metasploit 
+
+#installs extra stuff
+wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_85.0.4183.83-1_amd64.deb
+sudo dpkg -i google-chrome-stable_85.0.4183.83-1_amd64.deb
+
+#installs 3rd party apps
+yay -S keybase-bin
 
 #prints end of program
 clear
-figlet "done"
+figlet 
