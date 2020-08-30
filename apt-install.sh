a962955764d95325709d210bf93bae6e8b81f6f4
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-08-30 16:13:55 +0000

    Update apt-install.sh

--- apt-install.sh
+++ apt-install.sh
@@ -4,13 +4,22 @@ sudo apt-get update -y
 sudo apt-ger upgrade -y
 
 #to install my packages
-sudo apt-get install bluefish vokoscreen openbox obconf wireshark aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox lxpanel irssi
-sudo apt-get install firefox tor-browser-launcher nano vim sudo open-ssh-server python3-pip git npm hydra networkmanager -y
-sudo apt-get install cupp dirbuster firejail
+sudo apt-get install bluefish vokoscreen xmonad xmobar wireshark aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox irssi
+sudo apt-get install firefox tor-browser-launcher nano vim sudo openssh-server python3-pip git hydra networkmanager -y
+sudo apt-get install cupp dirbuster firejail 
 
 #makes sure your new packages are up to date
 sudo apt-get update
 sudo apt-get upgrade
+
+#installs chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd.deb
+
+#installs keybase
+curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+sudo apt install ./keybase_amd64.deb
+sudo apt --fix-broken install
 
 #prints end of program
 clear
