94206c75fa0b4dbd2a3a11c421c0af15a420b6f7
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-11-29 08:12:34 +0000

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,19 +6,23 @@ clear
 
 #this will install all the pkg and apt packages needed
 apt install sudo 
+apt install wget
+apt install curl
 apt install git 
 apt install openssh 
 apt install nmap 
 apt install python
-apt install python2
 apt install python3
 apt install python3-pip
 apt install figlet
+apt install vim 
+apt update 
+apt upgrade
 #this will install all the git clones needed
-git clone 
-git clone 
-git clone 
-git clone 
+#git clone 
+#git clone 
+#git clone 
+#git clone 
 
 #this install routersploit
 git clone https://github.com/threat9/routersploit.git
@@ -32,4 +36,24 @@ chmod +x metasploit.sh
 
 #this should end off the program with no errors
 clear
-figlet "done"
+figlet "done with half"
+sleep 1
+clear 
+figlet "next part"
+
+#installs kali nethunter
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+echo "this will be an iteractive install"
+echo "stand by"
+sleep 2
+./install-nethunter-termux
+
+exit
+exit 
+exit
+exit
+
+#this program is now done
+clear
+echo "done"
